89114f34f32182d816764015157a95ad06c47bad
bridgelancer <thchoy@hotmail.com>
2020-02-23 02:11:16 +0800

    Handle jsonified event with boardUpdate

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
+import json
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
+# Standard setup for Flask with SocketIO wrapping
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+# cors_allowed_origins options used for simplicity in testing
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
     print('One client connected...')
+
     emit('confirm connect', {})
     emit('my response', {'data': 'Server saying hi'})
 
+@socketio.on('boardUpdate', namespace='/test')
+def broadcast_update(event):
+    emit('boardUpdated', event, broadcast=True, include_self=False)
+    print('Board update broadcasted')
+
 @socketio.on('my event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']})
+def test_message(event):
+    print(f'Message recived from client: {event["data"]}')
+    emit('my response', {'data': event['data']})
 
 @socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+def test_broadcast_message(event):
+    print(f'Broadcast message recived from client: {event["data"]}')
+    emit('my response', {'data': event['data']}, broadcast=True)
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
